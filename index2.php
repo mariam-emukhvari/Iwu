f74c1c65bdbd8f7db67ed33393ed628db509cf8f
Mariami <mariam.emukhvari.1@btu.edu.ge>
2020-11-23 23:09:37 +0400

    Iwu

---
+++ index2.php
@@ -0,0 +1,9 @@
+CREATE TABLE db(
+db_name varchar(256) not null,
+db_pic varchar(256) not null,
+db_author varchar(256) not null,
+db_total_likes varchar(256) not null,
+db_comments varchar(256) not null,
+);
+
+INSERT INTO db (db_pic, db_author, db_total_liks, db_comments);
